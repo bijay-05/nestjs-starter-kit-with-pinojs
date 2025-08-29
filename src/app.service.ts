f405686306362616a258869cd700a12aac7bee31
bijay-05 <hawkeyesniper69@gmail.com>
2025-08-29 23:57:49 +0545

    root app module

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,22 +1,14 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { PinoLogger, InjectPinoLogger } from 'nestjs-pino';
 
 @Injectable()
 export class AppService {
-  constructor(
-    private readonly logger: PinoLogger,
-    // private readonly
-  ){
-    this.logger.setContext(AppService.name);
-  }
+  constructor(){}
   getHello(): string {
-    this.logger.error("First Info Log from PinoLogger.");
     return 'Hello World!';
   }
 
   getInfo(): string {
     throw new NotFoundException('Not found error');
-    this.logger.error("Error log from Info Service");
     return 'This is your info.'
   }
 
